c3a8ee893c8cebe658ba79dbd40df932b40867d2
cesardeazevedo <cesardeazevedo@outlook.com>
2015-07-14 20:40:04 -0300

    moved project to github

---
+++ www/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.honeycombs').honeycombs({
+        combWidth: 220,
+        margin: 10
+    });
+
+    var scrollTo = function(to) {
+        $.scrollTo(to, 1000, {easing:'easeInOutExpo', offset:0, 'axis':'y'});
+    };
+
+    $('.home--btn').click(function(e){
+        scrollTo('body');
+        e.preventDefault();
+    });
+    $('.about--btn').click(function(e){
+        scrollTo('#about');
+        e.preventDefault();
+    });
+    $('.team--btn').click(function(e){
+        scrollTo('#team');
+        e.preventDefault();
+    });
+});
